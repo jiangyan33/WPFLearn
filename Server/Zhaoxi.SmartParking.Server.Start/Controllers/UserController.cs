3288f066aaeeb9e0587ec96d05a9fae0cfae2f00
shijiangshan <shijiangshan@bjknrt.com>
2022-06-29 23:44:03 +0800

    保存

---
+++ Server/Zhaoxi.SmartParking.Server.Start/Controllers/UserController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Zhaoxi.SmartParking.Server.Start.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpPost("login")]
+        public string Login([FromForm] string userName, [FromForm] string password)
+        {
+            return "{\"State\":\"True\"}";
+        }
+    }
+}
